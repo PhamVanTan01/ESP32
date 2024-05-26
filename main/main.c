faeb53613489b7942ddf8a108132774a8891bd52
phamvantan <tanpham070@gmail.com>
2024-05-26 12:23:33 +0700

    update task 2 create a RGB_LED

--- main/main.c
+++ main/main.c
@@ -1,11 +1,23 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <unistd.h>
+#include "freertos/FreeRTOS.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+
+#include "rgb_led.h"
 
 void app_main(void)
 {
-    while (true) {
-        printf("Hello from app_main!\nsdfasdfasdfasdf");
-        sleep(1);
+    while (true)
+    {
+    	rgb_led_wifi_app_started();
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        rgb_led_http_server_started();
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        rgb_led_wifi_connected();
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
+
